6bd72c9018b776e43264c592fc67186537cdc67b
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-08-21 19:49:17 +0530

    JS operators done

---
+++ JavaScript/operators.js
@@ -0,0 +1,8 @@
+// checking the data-types using typeof operator
+console.log(typeof 42); // number
+console.log(typeof 'Hello'); // string
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (this is a known JavaScript quirk)
+console.log(typeof Symbol('sym')); // symbol
+console.log(typeof { name: 'Alice' }); // object
