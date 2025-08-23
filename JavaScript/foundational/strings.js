361aed8081eff062af2b18868d831f0f2d1ce85a
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-08-23 11:17:46 +0530

    Substring manipulation started

--- JavaScript/foundational/strings.js
+++ JavaScript/foundational/strings.js
@@ -1,7 +1,7 @@
-// impplementing the string literal
+// implementing the substring methods and it's variants
 
-let num1 = 10;
-let num2 = 20;
 
-let result = num1 + num2;
-console.log(`The sum of ${num1} and ${num2} is ${result}`);
+
+let sentence = "JavaScript is awesome!";
+let position = sentence.indexOf("fantastic!");
+console.log(position); // 14
