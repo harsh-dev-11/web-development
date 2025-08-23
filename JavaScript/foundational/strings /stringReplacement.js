fc3d7056b488dbfc8cf1dee859106a18b61ded56
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-08-23 19:39:25 +0530

    String replacement done

---
+++ JavaScript/foundational/strings /stringReplacement.js
@@ -0,0 +1,5 @@
+let sentence = "Hello, world!";
+console.log(sentence); // Output: Hello, world!
+// Replace "world" with "JavaScript"
+let newSentence = sentence.replace("world", "JavaScript");
+console.log(newSentence); // Output: Hello, JavaScript!
