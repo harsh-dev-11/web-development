45e400361720c2ac1d2e59472e8d8e897aeedc24
Harsh Dhiman <harshdhiman7044@gmail.com>
2025-08-24 20:32:02 +0530

    Number datatype cleared

---
+++ JavaScript/foundational/boolean and numbers/script.js
@@ -0,0 +1,7 @@
+let infinityNumber = 1 / 0;
+console.log(infinityNumber); 
+console.log(typeof infinityNumber);
+
+let notANumber = "string" / 2;
+console.log(notANumber);
+console.log(typeof notANumber);
