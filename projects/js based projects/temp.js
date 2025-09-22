69f6518e279f02019d61449cb46c4f59b5bee35d
Harsh Dhiman <108452157+h-dhiman08@users.noreply.github.com>
2025-09-22 11:17:54 +0000

    mars story added

--- projects/js based projects/temp.js
+++
@@ -1,8 +0,0 @@
-let originalArray = [1, 2, 3];
-// let copyArray = [].concat(originalArray);
-let copyArray = originalArray;
-
-originalArray.push(4);
-
-console.log(copyArray);
-console.log(originalArray);
