6db482f07fec304a4e2aea5d79443ccdd5cffe9a
Harsh Dhiman <108452157+h-dhiman08@users.noreply.github.com>
2025-09-29 19:20:30 +0530

    Added the icon and title to the portfolio

--- JavaScript/arrays/basics.js
+++ JavaScript/arrays/basics.js
@@ -1,4 +1,8 @@
-let fruits = ["apple", "banana", "cherry"];
-console.log(fruits);
-fruits[10] = "blueberry";
-console.log(fruits); 
+// copying nested arrays and objects
+
+let arr1 = [1, 2, 3, [4, 5]];
+let arr2 = arr1.slice(); // shallow copy using slice
+arr2[3][0] = 99; // modifying nested array in arr2
+arr2[0] = 100; // modifying primitive value in arr2
+console.log(arr1); // arr1 is affected due to shallow copy
+console.log(arr2); // arr2 reflects the change
---
+++ JavaScript/objects/basics.js
@@ -0,0 +1,4 @@
+let marks = 95;
+marks = 97;
+
+console.log(marks); 
