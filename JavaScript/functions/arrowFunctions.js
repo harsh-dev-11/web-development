e091b1f9bb9b733becdf5fe9ba302220579f4265
Harsh Dhiman <108452157+h-dhiman08@users.noreply.github.com>
2025-10-12 17:08:20 +0530

    Arrow function adde

---
+++ JavaScript/functions/arrowFunctions.js
@@ -0,0 +1,4 @@
+// creating a function that checks the divisibility of a number by 3
+const num = 3;
+const isDivisibleBy3 = (n) => ((n % num) == 0) ? true : false;
+console.log(isDivisibleBy3(9)); 
