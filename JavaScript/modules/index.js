38448f5f7e87644a9e3a1ced852d2291ff5427a4
Harsh Dhiman <108452157+h-dhiman08@users.noreply.github.com>
2025-10-21 16:37:44 +0530

    Modules part completed

---
+++ JavaScript/modules/index.js
@@ -0,0 +1,5 @@
+import utilityModule from './utils.js';
+
+const { add, print, subtract } = utilityModule;
+
+print(add(5, 3));
---
+++ JavaScript/modules/utils.js
@@ -0,0 +1,11 @@
+// implementing arrow functions
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const print = (value) => console.log(value);
+
+export default {
+    add,
+    subtract,
+    print
+}
