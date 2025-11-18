83b069ef3f11942dbf461a15bc6f57be16e144f1
Harsh Dhiman <108452157+h-dhiman08@users.noreply.github.com>
2025-11-18 15:27:38 +0530

    Random Background Color Changer done

---
+++ workshops and labs/js based/randomBackgroundColorChanger/script.js
@@ -0,0 +1,31 @@
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
+}
+console.log(getRandomIndex());
+
+const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.getElementById("bg-hex-code");
+console.log(bgHexCodeSpanElement);
+
+function changeBackgroundColor() {
+  bgHexCodeSpanElement.innerText = darkColorsArr[getRandomIndex()];
+  body.style.backgroundColor = darkColorsArr[getRandomIndex()];
+}
+const btn = document.querySelector("#btn");
+console.log(btn);
+
+btn.addEventListener("click", changeBackgroundColor);
