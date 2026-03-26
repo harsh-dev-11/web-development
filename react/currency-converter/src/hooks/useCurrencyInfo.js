8b4e2bbca7081ab6519fb850ba1abed566436f61
Harsh Kumar <harshdhiman7044@gmail.com>
2026-03-26 17:26:57 +0530

    currency converter done

---
+++ react/currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency = "usd") {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`,
+    )
+      .then((res) => res.json())
+      .then((currData) => {
+        setData(currData[currency]);
+      });
+    console.log(data);
+  }, [currency]);
+  return data;
+}
+
+export default useCurrencyInfo;
