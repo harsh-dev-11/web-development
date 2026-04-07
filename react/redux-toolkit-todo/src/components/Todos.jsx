eca6048e32ff4804c3e6f1ccf9bfa12d23bd14b3
Harsh Dhiman <harshdhiman7044@gmail.com>
2026-04-07 08:54:37 +0530

    Redux toolkit todo project done

--- react/redux-toolkit-todo/src/components/Todos.jsx
+++ react/redux-toolkit-todo/src/components/Todos.jsx
@@ -11,18 +11,22 @@ function Todos() {
       {todos.map((todo) => (
         <li
           key={todo.id}
-          className="flex justify-between items-center bg-gray-800 px-4 py-2 rounded-lg"
+          className="flex justify-between items-center bg-gray-800 px-4 py-2 rounded-lg 
+                 transition-all duration-300 hover:bg-gray-800 hover:scale-[1.02]"
         >
           <span className="text-gray-200">{todo.text}</span>
+
           <button
             onClick={() => dispatch(removeTodo(todo.id))}
-            className="text-red-400 hover:text-red-600 font-bold"
+            className="p-1 rounded-md transition-all duration-200 
+                   hover:bg-red-500/20 active:scale-90"
           >
             <img
-              src="https://img.icons8.com/3d-fluency/94/trash.png"
-              width="25"
+              width="20"
               height="20"
-              alt="trash"
+              src="https://img.icons8.com/office/40/multiply.png"
+              alt="multiply"
+              className="transition-all duration-200 hover:scale-110 hover:brightness-125"
             />
           </button>
         </li>
